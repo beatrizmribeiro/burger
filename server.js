25eae8ec2bd23826faff80ec4ef57a4fc4d2b98b
Beatriz Ribeiro <beatriz_mribeiro@hotmail.com>
2017-11-18 14:07:47 -0800

    test port 80

--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 var express = require("express");
 var bodyParser = require("body-parser");
-
+ 
 var methodOverride = require("method-override");
 
-var PORT = process.env.PORT || 3000;
+//var PORT = process.env.PORT || 3000;
+var PORT = 80;
 
 var app = express();
 
