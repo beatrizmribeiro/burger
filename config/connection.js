09a4cf7a0cc66f8c0ce17e4dd8ef9e33cb59fd91
Beatriz Ribeiro <beatriz_mribeiro@hotmail.com>
2017-11-18 04:43:46 -0800

    fixing

--- config/connection.js
+++ config/connection.js
@@ -2,7 +2,7 @@ var mysql = require("mysql");
 
 var connection = mysql.createConnection({
 	port:3306,
-	host:"127.0.0.1",
+	host:"localhost",
 	user: "root",
 	password: "",
 	database:"burgers_db"
